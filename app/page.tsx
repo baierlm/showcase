f0c11c288c18b20da85063e271768837bab74e85
Mark Baierl <baierlmark@gmail.com>
2025-11-16 18:53:49 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,191 @@
-import Image from "next/image";
-
-export default function Home() {
+"use client";
+import React from "react";
+import { HeroParallax } from "../components/ui/hero-parallax";
+import { WavyBackground } from "@/components/ui/wavy-background";
+import { Timeline } from "@/components/ui/timeline";
+import { AnimatedTestimonials } from "@/components/ui/animated-testimonials";
+import { Vortex } from "@/components/ui/vortex";
+export default function HeroParallaxDemo() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div>
+      <div className="w-full mx-auto rounded-md  h-screen overflow-hidden">
+        <Vortex
+          backgroundColor="black"
+          className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
+        >
+          <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
+            Tryhard Competition (THC)
+          </h2>
+          <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
+            Das beste teambasierte Kartenspiel der Welt
           </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+          <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
+            <button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
+              Jetzt Kaufen{" "}
+            </button>
+          </div>
+        </Vortex>
+      </div>
+      <HeroParallax products={products} />
+      <Timeline data={data} />
+      <AnimatedTestimonials testimonials={testimonials} />;
     </div>
   );
 }
+
+const data = [
+  {
+    title: "November 2025",
+    content: (
+      <div>
+        <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
+          Entscheidung getroffen, das Spiel zu veröffentlichen und auf den Markt
+          zu bringen.
+        </p>
+      </div>
+    ),
+  },
+  {
+    title: "2024",
+    content: (
+      <div>
+        <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
+          Immer wieder Späße darüber gemacht eine Firma zu gründen.
+        </p>
+      </div>
+    ),
+  },
+  {
+    title: "2023",
+    content: (
+      <div>
+        <p className="mb-4 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
+          Erste Orden veröffentlicht.
+        </p>
+      </div>
+    ),
+  },
+  {
+    title: "2022",
+    content: (
+      <div>
+        <p className="mb-4 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
+          Erstes Deck veröffentlicht.
+        </p>
+      </div>
+    ),
+  },
+];
+
+export const products = [
+  {
+    title: "14 Phoenix",
+    link: "",
+    thumbnail: "/images/14_phoenix.png",
+  },
+  {
+    title: "1395 Dragon",
+    link: "",
+    thumbnail: "/images/1395_dragon.png",
+  },
+  {
+    title: "Ass Bomben 1",
+    link: "",
+    thumbnail: "/images/ass_bomben_1.png",
+  },
+  {
+    title: "blindfolded",
+    link: "",
+    thumbnail: "/images/blindfolded-360-dog.png",
+  },
+  {
+    title: "Destroyer",
+    link: "",
+    thumbnail: "/images/destroyer_s_09.png",
+  },
+  {
+    title: "Dorftrottel",
+    link: "",
+    thumbnail: "/images/dorftrottel_s_02.png",
+  },
+  {
+    title: "Gambler",
+    link: "",
+    thumbnail: "/images/gambler_g_09.png",
+  },
+  {
+    title: "heelflip",
+    link: "",
+    thumbnail: "/images/heelflip_phoenix.png",
+  },
+  {
+    title: "kickflip",
+    link: "",
+    thumbnail: "/images/kickflip_dragon.png",
+  },
+  {
+    title: "treppe",
+    link: "",
+    thumbnail: "/images/lange_treppe_b_09.png",
+  },
+  {
+    title: "olli",
+    link: "",
+    thumbnail: "/images/olli_mahjong.png",
+  },
+  {
+    title: "pay2win",
+    link: "",
+    thumbnail: "/images/pay-to-win-s-A.png",
+  },
+  {
+    title: "speed",
+    link: "",
+    thumbnail: "/images/speed_dog.png",
+  },
+  {
+    title: "tornado",
+    link: "",
+    thumbnail: "/images/tornado_g_10.png",
+  },
+  {
+    title: "Vulkan",
+    link: "",
+    thumbnail: "/images/vulkan_r_10.png",
+  },
+  {
+    title: "wackelturm",
+    link: "",
+    thumbnail: "/images/wackelturm_r_09.png",
+  },
+];
+const testimonials = [
+  {
+    quote:
+      "Ich bin vielleicht voreingenomen, aber ich würde es 10/10 empfehlen.",
+    name: "Mark",
+    designation: "CEO",
+    src: "testimonials/mark.png",
+  },
+  {
+    quote:
+      "Die Erfahrung war nahtlos und das Team war unglaublich unterstützend während des gesamten Prozesses.",
+    name: "Tommy",
+    designation: "Lead Product Developer",
+    src: "testimonials/tommy.png",
+  },
+  {
+    quote:
+      "Als jemand, der normalerweise keine Bewertungen schreibt, fühlte ich mich gezwungen, meine positive Erfahrung zu teilen.",
+    name: "Philipp",
+    designation: "Buchhalter",
+    src: "testimonials/philipp.png",
+  },
+  {
+    quote:
+      "Ich bin zwar sehr konservativ, aber dieses Produkt hat mich wirklich beeindruckt.",
+    name: "Laura",
+    designation: "Hobbyspieler",
+    src: "testimonials/laura.png",
+  },
+];
